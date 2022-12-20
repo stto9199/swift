113119581ef1ee56afba705dcec49cefb31bad5a
Ellie Shin <ellie_shin@apple.com>
2022-12-20 14:04:02 -0800

    Update test/diagnostics/package-name-diagnostics.swift

--- test/diagnostics/package-name-diagnostics.swift
+++ test/diagnostics/package-name-diagnostics.swift
@@ -1,6 +1,6 @@
 // RUN: %empty-directory(%t)
 
-// RUN: %target-swift-frontend -module-name Logging -package-name My-Logging%Pkg %t/File.swift -emit-module -emit-module-path %t/Logging.swiftmodule | %FileCheck %s -check-prefix CHECK-BAD
+// RUN: %target-swift-frontend -module-name Logging -package-name My-Logging%Pkg %s -emit-module -emit-module-path %t/Logging.swiftmodule | %FileCheck %s -check-prefix CHECK-BAD
 // CHECK-BAD: package name "My-Logging%Pkg" is not a valid identifier
 
 // RUN: %target-swift-frontend -module-name Logging -package-name Swift %t/File.swift -emit-module -emit-module-path %t/Logging.swiftmodule | %FileCheck %s -check-prefix CHECK-STDLIB
