6ac3331293e5f1223ed6c0c4e28dfe70c3260ea5
Ellie Shin <ellie_shin@apple.com>
2022-12-20 14:04:09 -0800

    Update test/diagnostics/package-name-diagnostics.swift

--- test/diagnostics/package-name-diagnostics.swift
+++ test/diagnostics/package-name-diagnostics.swift
@@ -3,7 +3,7 @@
 // RUN: %target-swift-frontend -module-name Logging -package-name My-Logging%Pkg %s -emit-module -emit-module-path %t/Logging.swiftmodule | %FileCheck %s -check-prefix CHECK-BAD
 // CHECK-BAD: package name "My-Logging%Pkg" is not a valid identifier
 
-// RUN: %target-swift-frontend -module-name Logging -package-name Swift %t/File.swift -emit-module -emit-module-path %t/Logging.swiftmodule | %FileCheck %s -check-prefix CHECK-STDLIB
+// RUN: %target-swift-frontend -module-name Logging -package-name Swift %s -emit-module -emit-module-path %t/Logging.swiftmodule | %FileCheck %s -check-prefix CHECK-STDLIB
 // CHECK-STDLIB: package name "Swift" is reserved for the standard library
 
 
